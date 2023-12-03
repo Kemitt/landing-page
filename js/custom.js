936127a103fd21ccb0df5404441e186be6487ac3
AyaKemitt <aya.rabee@kemitt.com>
2023-12-03 16:29:03 +0200

    update in ui color and logo

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav a, .contactus a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    const offset = 60; 
+
+    window.scrollTo({
+        top: targetSection.offsetTop - offset,
+        behavior: 'smooth'
+    });
+    });
+});
