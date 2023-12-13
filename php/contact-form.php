3ba77cb14b6ab5f0984650cf5e79d006face7616
AyaKemitt <aya.rabee@kemitt.com>
2023-12-14 01:44:55 +0200

    change in contact form

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,48 +1,98 @@
 <?php
- 
+
+namespace PortoContactForm;
+
+session_cache_limiter('nocache');
+header('Expires: ' . gmdate('r', 0));
+
+header('Content-type: application/json');
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'php-mailer/src/PHPMailer.php';
 require 'php-mailer/src/SMTP.php';
 require 'php-mailer/src/Exception.php';
- 
-//Create an instance; passing `true` enables exceptions
-if (isset($_POST["send"])) {
- 
-  $mail = new PHPMailer(true);
- 
-    //Server settings
-    $mail->isSMTP();                              //Send using SMTP
-    $mail->Host       = 'smtpout.asia.secureserver.net'; 	       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;             //Enable SMTP authentication
-    $mail->Username   = 'info@eqtaralawla.com';    //SMTP write your email
-    $mail->Password   = 'sa#87654321';        //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit SSL encryption
-    $mail->Port       = 465;                                    
 
-    //Recipients
-    $mail->setFrom( $_POST["email"], $_POST["name"]); // Sender Email and name
-    $mail->addAddress('nfo@eqtaralawla.com');     //Add a recipient email  
-    $mail->addReplyTo($_POST["email"], $_POST["name"]); // reply to sender email
-     
-    //multiple file attachment
-  for ($i = 0; $i < count($_FILES['file']['tmp_name']); $i++) {
-     $mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i] ); 
-  }
- 
-    //Content
-    $mail->isHTML(true);               //Set email format to HTML
-    $mail->Subject = 'Received New Message From: '. $_POST["name"];   // email subject headings
-    $mail->Body    = $_POST["message"]; //email message
-      
-    // Success sent message alert
-    $mail->send();
-    echo
-    " 
-    <script> 
-     alert('Message was sent successfully!');
-     document.location.href = 'contact-form.php';
-    </script>
-    ";
+
+// Step 1 - Enter your email address below.
+$email = 'info@eqtaralawla.com';
+
+// If the e-mail is not working, change the debug option to 2 | $debug = 2;
+$debug = 2;
+
+// If contact form don't has the subject input change the value of subject here
+$subject = 'General Inquiry';
+
+$message = "";
+
+foreach($_POST as $label => $value) {
+	$label = ucwords($label);
+
+	// Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
+
+	// if( $label == 'Email' ) {               
+	// 	$label = 'Email Address';              
+	// }
+
+	// Checkboxes
+	if( is_array($value) ) {
+		// Store new value
+		$value = implode(', ', $value);
+	}
+
+	$message .= $label.": " . nl2br(htmlspecialchars($value, ENT_QUOTES)) . "<br>";
 }
+
+$mail = new PHPMailer(true);
+
+try {
+
+	$mail->SMTPDebug = $debug;                                 // Debug Mode
+
+	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
+
+	$mail->IsSMTP();                                         // Set mailer to use SMTP
+	$mail->Host = 'smtpout.secureserver.net'; 		       // Specify main and backup server
+	$mail->SMTPAuth = true;                                  // Enable SMTP authentication
+	$mail->Username = 'info@eqtaralawla.com';                   // SMTP username
+	$mail->Password = 'sa#87654321';                              // SMTP password
+	$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
+	$mail->Port = 587;  								       // TCP port to connect to
+
+	$mail->AddAddress($email);	 						       // Add another recipient
+
+	//$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
+	//$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
+	//$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
+
+	// From - Name
+	$fromName = ( isset($_POST['name']) ) ? $_POST['name'] : 'Website User';
+	$mail->SetFrom($email, $fromName);
+
+	// Reply To
+	if( isset($_POST['email']) && !empty($_POST['email']) ) {
+		$mail->AddReplyTo($_POST['email'], $fromName);
+	}
+
+	$mail->IsHTML(true);                                       // Set email format to HTML
+
+	$mail->CharSet = 'UTF-8';
+
+	$mail->Subject = $subject;
+	$mail->Body    = $message;
+
+	$mail->Send();
+	$arrResult = array ('response'=>'success');
+
+} catch (Exception $e) {
+	$arrResult = array ('response'=>'error','errorMessage'=>$e->errorMessage());
+} catch (\Exception $e) {
+	$arrResult = array ('response'=>'error','errorMessage'=>$e->getMessage());
+}
+
+if ($debug == 0) {
+	echo json_encode($arrResult);
+}
+
+?>
